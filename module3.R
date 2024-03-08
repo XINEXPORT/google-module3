6c35e82b41ed99ab8a05d11cdbb80f74b369512a
Christine Hoang <christinehoangt@gmail.com>
2024-03-08 12:38:13 -0600

    save

--- module3.R
+++ module3.R
@@ -1,1 +1,45 @@
 install.packages("here")
+
+library("here")
+
+install.packages("skimr")
+
+library("skimr")
+
+install.packages("janitor")
+
+library("janitor")
+
+install.packages("dplyr")
+
+library("dplyr")
+
+install.packages("palmerpenguins")
+
+library("palmerpenguins")
+
+skim_without_charts(penguins)
+
+glimpse(penguins)
+
+head(penguins)
+
+penguins %>%
+  select (-species)
+
+penguins %>%
+  rename(island_new=island)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
